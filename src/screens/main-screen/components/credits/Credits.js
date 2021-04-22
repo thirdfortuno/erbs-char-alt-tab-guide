5e4ce4071aa5564c64683d0739fef6616efb899b
thirdfortuno <thirdfort@gmail.com>
2021-04-22 13:31:51 +0800

    favicon and credits

--- src/screens/main-screen/components/credits/Credits.js
+++ src/screens/main-screen/components/credits/Credits.js
@@ -1,12 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { db } from "../../../../firebase";
 
 const Credits = () => {
+  const [writers, setWriters] = useState("");
+
+  const creditsWriters = async () => {
+    let string = "";
+
+    const snapshot = await db.collection("credits").get();
+
+    snapshot.forEach((doc) => (string = string + doc.data().name + ", "));
+
+    setWriters(string.substring(0, string.length - 2));
+  };
+
+  useEffect(() => {
+    creditsWriters();
+  }, []);
+
   return (
     <div>
+      <hr />
       <p>
         Thanks to my friends, the devs, and the discord channel for looking at
         this while i was developing this
       </p>
+      <p>Website created by Archanist!</p>
+      <p>Special thanks to tip writers: {writers}</p>
     </div>
   );
 };
